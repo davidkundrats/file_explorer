da24232bea8abdbc68e64bf64cb073231ebdd48a
davidkundrats <davidkundrats@gmail.com>
2023-10-08 23:04:55 -0400

    added documentation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream> 
+#include <iostream>
 #include <filesystem>
 #include "navigation.h"
 
+// Main function, calls switch_logic() from navigation.h
 
-int main() 
-{ 
-    switch_logic(); 
-    return 0; 
+int main()
+{
+    switch_logic();
+    return 0;
 }
